8abb90d1db3e0ab07964fb624ee876b350d6b7f5
Domchap90 <domchaple@gmail.com>
2020-04-12 13:35:33 +0000

    Second phase of testing complete- age ranges working.

--- spec/whatCanIDrinkSpec.js
+++ spec/whatCanIDrinkSpec.js
@@ -1,22 +1,22 @@
 describe ("whatCanIDrink", function() {
     
     it("should return `Sorry. I can’t tell what drink because that age is incorrect!`", function(){
-        expect(whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        expect(whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");//less than  0
     });
     it("should return `Drink Toddy`", function(){
-        expect(whatCanIDrink(13)).toBe("Drink Toddy");
+        expect(whatCanIDrink(1)).toBe("Drink Toddy");  // less than 14
     });
     it("should return `Drink Coke`", function(){
-        expect(whatCanIDrink(17)).toBe("Drink Coke");
+        expect(whatCanIDrink(14)).toBe("Drink Coke");   // less than 18
     });
     it("should return `Drink Beer`", function(){
-        expect(whatCanIDrink(20)).toBe("Drink Beer");
+        expect(whatCanIDrink(18)).toBe("Drink Beer");   // less than 21
     });
     it("should return `Drink Whisky`", function(){
-        expect(whatCanIDrink(129)).toBe("Drink Whisky");
+        expect(whatCanIDrink(21)).toBe("Drink Whisky");    // less than 130
     });
     it("should return `Sorry. I can’t tell what drink because that age is incorrect!`", function(){
-        expect(whatCanIDrink(135)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        expect(whatCanIDrink(130)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");   // 130+
     });
 
 });
