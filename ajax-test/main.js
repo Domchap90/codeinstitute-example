eac1d8440b1a6176209789ac0edca993a3d1c051
Domchap90 <domchaple@gmail.com>
2020-04-14 17:13:25 +0000

    Table correctly formatted with commas removed using regex.Type parameter switched for url.

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,10 +1,10 @@
 
-const baseURL = 'https://ci-swapi.herokuapp.com/api/';
+const url = 'https://ci-swapi.herokuapp.com/api/';
 
-function getData(type, cb) {
+function getData(url, cb) {
 var xhr = new XMLHttpRequest(); 
               
-xhr.open('GET', baseURL + type + '/');
+xhr.open('GET', url);
 xhr.send();                    
 
 xhr.onreadystatechange = function() {
@@ -18,30 +18,47 @@ function getTableHeaders(obj) {
     var tableHeaders = [];
 
     Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td>${key}</td>`);
+        tableHeaders.push(`<td>${key}</td>`);   // gets all table headers in array.
     });
-    return `<tr>${tableHeaders}</tr>`;
+    return `<tr>${tableHeaders}</tr>`; // returns them in a single row.
 }
 
+function generatePaginationButtons(next, prev) {
+    if(next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+        } else if (next && !prev) {
+            return `<button onclick="writeToDocument('${next}')">Next</button>`;
+        } else {
+            return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+        }
+    
+}
 
-function writeToDocument(type){
-    var tableRows = [];
+function writeToDocument(url){
+    var tableRows = [];     // initiates row for values associated with key in JSON.
     var el = document.getElementById("data");
-    el.innerHTML = "";
+    el.innerHTML = "";  // clears preexisting data before adding more to the current row.
 
-    getData(type, function(data){
+    getData(url, function(data){
+        var pagination;
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next,data.previous)
+        }
         data = data.results;
         //console.dir(data);
-        var tableHeaders = getTableHeaders(data[0]);
+        var tableHeaders = getTableHeaders(data[0]);    // calls function to get headers of table for data in the div.
 
         data.forEach(function(item){
             var dataRow = [];
 
             Object.keys(item).forEach(function(key){
-                dataRow.push(`<td>${item[key]}</td>`); // creates row.
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0,15);    // formats so nicely fits in column - all same size.
+                dataRow.push(`<td>${truncatedData}</td>`); // creates row.
             })
-            tableRows.push(dataRow);
+            tableRows.push(`<tr>${dataRow}</tr>`); // push row to array
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
